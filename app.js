3faf4221171d7957ac3da5ed000212ba79ffd721
Zac Atsatt <contact@ujlee.com>
2016-03-09 19:48:20 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const PORT = 3340;
+const transFilename = './transactions.json';
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var uuid = require('uuid');
+
+const totalFilename = path.join(__dirname, './total.json');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  var indexPath = path.join(__dirname, 'index.html');
+  res.sendFile(indexPath);
+});
+
+app.get('/style.css', function(req, res, next){
+  res.sendFile(path.join(__dirname, './style.css'));
+});
+
+app.get('/main.js', function(req, res, next){
+  res.sendFile(path.join(__dirname, './main.js'));
+});
+
+app.get('/total', function(req, res, next){
+  res.sendFile(totalFilename);
+});
+
+app.post('/total', function(req, res) {
+  console.log(req.body);
+  fs.writeFile(totalFilename, JSON.stringify(req.body), function(err) {
+    console.error('totalPOST err: ', err);
+    res.end();
+    });
+  })
+
+app.get('/trans', function(req, res) {
+  fs.readFile(transFilename, function(err, data) {
+    var trans = JSON.parse(data);
+    res.send(trans);
+  });
+});
+
+app.post('/trans', function(req, res) {
+  fs.readFile(transFilename, function(err, data) {
+    var trans = JSON.parse(data);
+    var newTrans = req.body;
+    newTrans.id = uuid.v1();
+    trans.push(newTrans);
+  fs.writeFile(transFilename, JSON.stringify(trans), function(err) {
+    console.error(err);
+    res.end();
+    });
+  })
+})
+
+app.delete('/trans/:id', function (req, res) {
+  var id = req.params.id;
+  fs.readFile(transFilename, function(err, data) {
+    var trans = JSON.parse(data);
+    var transaction = trans.find(function(obj) {
+      return obj.id === id;
+    });
+    trans.splice(transaction, 1);
+    fs.writeFile(transFilename, JSON.stringify(trans), function(err) {
+      console.error(err);
+      res.end();
+    });
+  })
+})
+
+var server = http.createServer(app);
+
+server.listen(PORT, function() {
+console.log(`Server listening on port ${PORT}`);
+})
---
+++ public/main.js
@@ -0,0 +1,83 @@
+'use strict';
+
+$(document).ready(function() {
+   $('select').material_select();
+ });
+
+ var snap = angular.module('snuffles', []);
+
+   snap.controller('mainCtrl', function($scope, $http) {
+
+     function getTrans() {
+       $http({
+         method: 'GET',
+         url: '/trans'
+       })
+       .then(function(res) {
+         $scope.trans = res.data;
+       }, function(err) {
+         console.error('error: ', err);
+       })
+     }
+
+   getTrans();
+
+   function getTotal() {
+     $http({
+       method: 'GET',
+       url: '/total'
+     })
+     .then(function(res) {
+       $scope.total = res.data.total;
+     }, function(err) {
+       console.error('error: ', err);
+     })
+   }
+   getTotal();
+
+     $scope.addTrans = function() {
+      if($scope.newTrans.type === 'none') {
+        alert("Please select transaction type.");
+      } else if($scope.newTrans.type === 'deposit'){
+        $scope.newTrans.type = 'Deposit'
+        $scope.newTrans.amount = ($scope.newTrans.amount).toFixed(2);
+
+        $scope.total = (parseFloat($scope.total) + parseFloat($scope.newTrans.amount));
+        postTrans($scope.newTrans, JSON.stringify($scope.total));
+        $scope.trans.push($scope.newTrans);
+        $scope.newTrans = {};
+      } else if ($scope.newTrans.type === 'withdraw') {
+        $scope.newTrans.type = 'Withdrawal'
+        $scope.newTrans.amount = ($scope.newTrans.amount).toFixed(2);
+
+        $scope.total = (parseFloat($scope.total) - parseFloat($scope.newTrans.amount));
+        console.log($scope.total);
+        postTrans($scope.newTrans, JSON.stringify($scope.total));
+        $scope.trans.push($scope.newTrans);
+        $scope.newTrans = {};
+     }
+     };
+
+     $scope.delTrans = function(trans) {
+       var delIndex = this.transaction.id;
+       $http({
+         method: 'DELETE',
+         url: `/trans/${delIndex}`,
+       });
+        var tran = $scope.trans.find(function(obj) {
+          return obj.id === delIndex;
+        });
+        console.log(tran);
+        $scope.total = (parseFloat($scope.total) - parseFloat(tran.amount));
+        $scope.trans.splice(tran, 1);
+     }
+
+     function postTrans(data, total) {
+       $http.post("/trans", data).success(function(data, status) {
+       })
+       $http.post("/total", total).success(function(total, status) {
+       })
+     };
+
+
+ });
